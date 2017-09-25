d241e823f54bcfbe61bdb079b856d780b1573dd4
svzj95 <svzj95@gmail.com>
2017-09-25 19:02:18 +0300

    Works only output parts

---
+++ src/main/webapp/my.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#hhd").click(function(){
+        $("#hd").slideToggle();
+    });
+    $("#hram").click(function(){
+        $("#ram").slideToggle();
+    });
+    $("#hpsu").click(function(){
+        $("#psu").slideToggle();
+    });
+    $("#hvc").click(function(){
+        $("#vc").slideToggle();
+    });
+});
