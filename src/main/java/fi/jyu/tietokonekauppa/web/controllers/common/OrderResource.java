218693ca9a164fb313cfff8ffa07b8610bc0cc99
Denis <royalruby@yandex.ru>
2017-09-30 02:09:22 +0300

    new method signatures

--- src/main/java/fi/jyu/tietokonekauppa/web/controllers/common/OrderResource.java
+++ src/main/java/fi/jyu/tietokonekauppa/web/controllers/common/OrderResource.java
@@ -1,9 +1,12 @@
 package fi.jyu.tietokonekauppa.web.controllers.common;
 
+import fi.jyu.tietokonekauppa.models.Component;
 import fi.jyu.tietokonekauppa.models.Order;
 import fi.jyu.tietokonekauppa.services.OrderService;
+import fi.jyu.tietokonekauppa.web.StringStatus;
 import fi.jyu.tietokonekauppa.web.exceptions.DataExistsException;
 import fi.jyu.tietokonekauppa.web.exceptions.DataNotFoundException;
+import fi.jyu.tietokonekauppa.web.exceptions.FormException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.ws.rs.*;
@@ -12,6 +15,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Path("/orders")
 public class OrderResource {
@@ -28,8 +35,19 @@ public class OrderResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addOrder (Order item, @Context UriInfo uriInfo){
+    public Response addOrder (@QueryParam("notes") String notes, List<Component> items, @Context UriInfo uriInfo){
+        System.out.println("DEBUG contents "+notes);
+        System.out.println("DEBUG items "+items);
+        if(notes == null){
+            List<String> errors = new ArrayList<String>() {{ add("form exception"); }};
+            Map<String, String[]> fields = new HashMap<String, String[]>() {{
+                if(notes == null) put("notes", new String[]{"not provided"});
+            }};
+            throw new FormException(errors, fields);
+        }
         // TODO rework method according to API reference
+        // TODO we should get get user from SecurityContext
+        /*
         if(item.getId() != null && orderService.isOrderExist(item)){
             throw new DataExistsException("Order already exists");
         }
@@ -41,5 +59,7 @@ public class OrderResource {
         String newId = String.valueOf(item.getId());
         URI uri = uriInfo.getAbsolutePathBuilder().path(newId).build();
         return Response.created(uri).entity(item).build();
+        */
+        return Response.ok().entity(new StringStatus("ok")).build();
     }
 }
